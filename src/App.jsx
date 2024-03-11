33091dd5931f3d110b5016d78cb1bf1de5b2d142
Thay <thaywane.oliveira@gmail.com>
2024-03-11 15:54:19 -0300

    codigo desafio 2

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './Style.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <h1>Desafio 2 - VNW</h1>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
